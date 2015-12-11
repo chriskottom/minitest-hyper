4ef5102608e85e2cb4dd6aa36c4c9fe387253d34
Chris Kottom <chris@chriskottom.com>
2015-12-11 15:08:53 +0100

    initial commit

---
+++ lib/minitest/hyper.rb
@@ -0,0 +1,7 @@
+require "minitest/hyper/version"
+
+module Minitest
+  module Hyper
+    # Your code goes here...
+  end
+end
---
+++ lib/minitest/hyper/version.rb
@@ -0,0 +1,5 @@
+module Minitest
+  module Hyper
+    VERSION = "0.1.0"
+  end
+end
